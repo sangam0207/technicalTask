cf9aed2dd42a21434ec2b2e092c5f4f2b3f97c1f
sangam <sangam@reeach.one>
2024-05-19 20:30:05 +0530

    Changess

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const{register,loginUser,sendOtpForForgetPassword,updatePassword}=require("../controllers/userController");
 router.post("/register",register);
 router.post("/login",loginUser);
+// api for forget password ! You can update the password via mail otp method 
 router.post("/sendOtp",sendOtpForForgetPassword);
 router.post("/resetPassword",updatePassword)
 module.exports = router;
